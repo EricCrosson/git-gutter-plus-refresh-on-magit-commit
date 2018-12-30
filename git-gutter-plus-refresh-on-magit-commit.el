e3d5080eb300171733df0b10b87c5c084d5f8803
Eric Crosson <eric.s.crosson@utexas.com>
2018-12-30 17:47:06 -0600

    Include version-numbers of dependencies
    They are rather strict and can probably be relaxed

--- git-gutter-plus-refresh-on-magit-commit.el
+++ git-gutter-plus-refresh-on-magit-commit.el
@@ -6,7 +6,7 @@
 ;; Version: 0.0.1
 ;; Keywords: convenience
 ;; URL: https://github.com/EricCrosson/git-gutter-plus-refresh-on-magit-commit
-;; Package-Requires: ((emacs "24") (projectile) (magit) (git-gutter+))
+;; Package-Requires: ((emacs "24") (projectile "1.0.0") (magit "2.1.0") (git-gutter+ "0.4"))
 ;;
 ;; This file is not a part of GNU Emacs.
 ;;
